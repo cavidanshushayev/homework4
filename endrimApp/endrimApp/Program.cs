8d32e62b6b8f76555bc4f8c70f03eb239f9cbe23
Cavidan Shushayev <csusayev19@gmail.com>
2023-02-25 08:46:09 +0400

    homework4 commit

---
+++ endrimApp/endrimApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.Design;
+
+namespace endrimApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("zehmet olmasa alish-verish meblegini daxil edin");
+            double mebleg = Convert.ToDouble(Console.ReadLine());
+            double endrimlimebleg;
+            if (mebleg < 100)
+            {
+                Console.WriteLine("tebrikler siz 5% endrim elde etdiniz");
+                endrimlimebleg = mebleg - mebleg * 5 / 100;
+                Console.WriteLine("endrimden sonra odemeli oldugunuz mebleg:" + endrimlimebleg);
+            }
+            else if (mebleg > 100 && mebleg < 1000)
+            {
+                Console.WriteLine("TEBRIKLER siz 8% endrim elde etdiniz");
+                endrimlimebleg = mebleg - mebleg * 8 / 100;
+                Console.WriteLine("endrimden sonra odemeli oldugunuz mebleg :" + endrimlimebleg);
+            }
+            else
+            {
+                Console.WriteLine("TEBRIKLER SIZ 10% ENDRIM QAZANDINIZ");
+                endrimlimebleg = mebleg - mebleg * 10 / 100;
+                Console.WriteLine("endrimden sonra odemeli oldugunuz mebleg:" + endrimlimebleg);
+            }
+
+
+
+        }
+    }
+}
